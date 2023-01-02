79e919245e1e0e83df4fd04cbd35acb043c53130
demien666 <dmitry.n.kovalsky@gmail.com>
2023-01-02 21:29:11 +0100

    Dev (#2)
    * initial version
    
    * Somehow working version
    
    * Playlist with PREV and NEXT buttons
    
    * added GRID layout for folders and root folders filter
    
    * BACK and HOME buttons
    
    * added .m4v extension
    
    * Updated readme
    
    Co-authored-by: dmitry <demien@3g.ua>

--- README.md
+++ README.md
@@ -1,9 +1,64 @@
 # stsmedserv
 Stupidly simple media server
 
+# What is it?
+I'm a big fan of KODI, but sometimes, for some reason it's not working. May be the problem is with samba implementation on my Raspberrypi4 which acts as NAS, who knows. So I desided to create this simple media server as a "backup" for KODI. 
 
-Convering: 
+# How it works?
+It's a web server which I'm running on NAS(raspberrypi4). It's exposing my video files via HTTP. So, using web browser like Google Chrome I can watch videos from my NAS.
 
-For webm (VP8 / Vorbis):
-ffmpeg -i S01E01_Pups.avi -vcodec libvpx -acodec libvorbis -f webm -g 30 S01E01_Pups-FF4.avi
+# Is it taking some "meta" information about video from internet?
+No, it's 100% "offline". The only option now is a picture for folder. If folder has a file: 
+```
+poster.jpg
+```
+it will be displayed. 
 
+# Some technical details
+It's a python application which use FLASK as web server. It's not recoding/decoding/encoding video, it's just exposing video to your browser and browser acts as "video player". If your video in some old format like divx/xvid - browser will not be able to play it. Modern formats like h.264 are working fine(at least in Google Chrome).   
+
+# Some screenshots
+Folders with videos(like "Season1", "Season2"...):
+![folder view](./docs/folder_view.png)
+
+And when we're clicking on some folder, we're drillind down to "file list view":  videos inside folder(like S01E01.avi, S01E02.avi...):
+![files view](./docs/file_view.png)
+
+And next, when we're clicking on some video, we're finally reaching "play window" in which video is starts playing. It also has Prev/Next buttons. At the end of video - it jumps automatically to next. 
+![play view](./docs/play_view.png)
+
+
+
+# How to run it? 
+- Download as zip or "git clone" this repo
+- Install python3 if you don't have it
+- Install pip for python if you don't have it
+- If you don't want to pollute your python installation - create virtual environment: 
+```
+python3 -m venv .env
+```
+and activate it:
+```
+source .venv/bin/activate
+```
+- Install dependensies (actually it's just a flask):
+```
+pip install -r requirements.txt
+```
+- Configuration:
+There are parameters in file stsmedserv.ini:
+    - base_path - mandatory parameter. It's a path to your video library.  
+    - root_folder_filter - optional parameter. In my case I have 2 different folders with videos: one for series and one for movies. The only way to show them is to configure "parent" for them as "base_path". But in this "parent" I have also many other folders which I don't want to show. This parameter is to provide just list of folders which should be shown (in my case it's 2 folders: movies and series). Folders should be separeted by coma (",").
+```
+[main]
+base_path = /home/dmitry
+# root_folder_filter = series,movies
+```    
+- Run server:
+```
+sh ./run.sh 
+```
+- Open your web browser:
+```
+http://hostname or ip addr:5000/
+```
--- stsmedserv.py
+++ stsmedserv.py
@@ -5,15 +5,17 @@ from dataclasses import dataclass
 import configparser
 
 config = configparser.ConfigParser()
-config.read('stsmedserv.ini')
+config.read("stsmedserv.ini")
 
 URI_PATH_SEP = "->"
 URI_SPACE = "%20"
 PATH_SEP = "/"
 BASE_PATH = config["main"]["base_path"]
-VID_EXT = [".avi", ".mkv", ".mp4"]
+ROOT_FOLDER_FILTER = config["main"].get("root_folder_filter", None)
+VID_EXT = [".avi", ".mkv", ".mp4", ".m4v"]
 
 app = Flask(__name__, static_folder=BASE_PATH)
+
 
 @dataclass
 class FileInfo:
@@ -24,29 +26,45 @@ class FileInfo:
     def __lt__(self, other):
         return self.name < other.name
 
-GLOBAL_FILES: List[FileInfo] = []       
+
+GLOBAL_FILES: List[FileInfo] = []
+
 
 def uri_to_path(uri: str) -> str:
     return uri.replace(URI_PATH_SEP, PATH_SEP).strip().replace(URI_SPACE, " ")
 
+
 def path_to_uri(path: str) -> str:
     uri = path.replace(PATH_SEP, URI_PATH_SEP).strip().replace(" ", URI_SPACE)
     if uri.startswith(URI_PATH_SEP):
-        return uri[len(URI_PATH_SEP):]
+        return uri[len(URI_PATH_SEP) :]
     else:
-        return uri    
+        return uri
+
 
 def is_video_file(name: str) -> bool:
     filtered = [v for v in VID_EXT if v in name]
     return len(filtered) > 0
 
+
 def remove_file_extension(name: str) -> str:
     result = name
     for ext in VID_EXT:
         result = result.replace(ext, "")
-    return result    
+    return result
 
-def get_folders_and_files(dir_path: str) -> Tuple[List[FileInfo], List[FileInfo]]:
+
+def upper_path(path: str) -> str:
+    parts = path.split(PATH_SEP)
+    if len(parts) == 1:
+        return path
+    head = parts[:-1]
+    return PATH_SEP.join(head)
+
+
+def get_folders_and_files(
+    dir_path: str, folder_filter: List[str]
+) -> Tuple[List[FileInfo], List[FileInfo]]:
     folders = []
     files = []
     for file_name in os.listdir(dir_path):
@@ -59,50 +77,86 @@ def get_folders_and_files(dir_path: str) -> Tuple[List[FileInfo], List[FileInfo]
                 files.append(file_info)
         else:
             file_info.uri = "../folder/" + path_to_uri(local_path)
-            folders.append(file_info)
+            if len(folder_filter) == 0 or file_name in folder_filter:
+                folders.append(file_info)
     folders.sort()
     files.sort()
     global GLOBAL_FILES
     GLOBAL_FILES = files
-    return folders, files         
+    return folders, files
+
+
+def folders_to_groups(folders: List[str]) -> List[List[str]]:
+    # print(f"getting groups for: {folders}")
+    GROUP_SIZE = 5
+    result = []
+    group = []
+    idx = 0
+    for folder in folders:
+        group.append(folder)
+        idx = idx + 1
+        if idx >= GROUP_SIZE:
+            idx = 0
+            result.append(group)
+            group = []
+    result.append(group)
+    return result
+
 
 @app.route("/")
 def hello_world():
-    return folder()
+    folder_filter = [] if ROOT_FOLDER_FILTER is None else ROOT_FOLDER_FILTER.split(",")
+    return folder("", folder_filter)
+
 
 @app.route("/play/<uri>")
 def play(uri):
     path = uri_to_path(uri)
-    print(f"URI: {uri}" )
-    print(f"Path: {path}" )
+    # print(f"URI: {uri}" )
+    # print(f"Path: {path}")
+    folder = upper_path(path)
+    folder_uri = path_to_uri(folder)
+    # print(f"Folder: {folder}, folder_uri={folder_uri}")
 
     files = GLOBAL_FILES
     index = 0
     i = 0
     for f in files:
-        f.real_path = uri_to_path(f.uri.replace("../play/",""))
-        f.uri = url_for('static', filename=f.real_path)
+        f.real_path = uri_to_path(f.uri.replace("../play/", ""))
+        f.uri = url_for("static", filename=f.real_path)
         # print(f"{path} VS {f.real_path}")
         if path in f.real_path:
             index = i
-        i = i+1
-    #print(f"Index: {index}")    
+        i = i + 1
+    # print(f"Index: {index}")
 
-    return render_template('video.html', video_path=path, files=files, index=index)
+    return render_template(
+        "video.html", video_path=path, files=files, index=index, folder_uri=folder_uri
+    )
+
 
 @app.route("/folder/<uri>")
-def folder(uri=""):
+def folder(uri="", folder_filter=[]):
     path = uri_to_path(uri)
-    full_path = os.path.join(BASE_PATH, path)
-    (folders, files) =  get_folders_and_files(full_path)
-    return render_template('folder.html', folders=folders, files=files, path=path)    
+    back_path = upper_path(path)
+    back_uri = None if len(uri) == 0 else path_to_uri(back_path)
+    # print(f"back_uri={back_uri}")
+    os_path = os.path.join(BASE_PATH, path)
+    (folders, files) = get_folders_and_files(os_path, folder_filter)
+    groups = folders_to_groups(folders)
+    # print(f"groups: {groups}")
+    return render_template(
+        "folder.html", groups=groups, files=files, path=path, back_uri=back_uri
+    )
 
 
 def main():
     print("hello!")
     folder("Downloads->Test->Dragon")
-    play("Downloads->Test->Dragon->Jedcy%20smokw%20Dziewi%20wiatw%20S02E03pl%20-%20CDA.mp4")
-    
+    play(
+        "Downloads->Test->Dragon->Jedcy%20smokw%20Dziewi%20wiatw%20S02E03pl%20-%20CDA.mp4"
+    )
+
 
 if __name__ == "__main__":
-    main()    
+    main()
